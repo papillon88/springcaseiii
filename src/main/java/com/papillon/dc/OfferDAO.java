35b831af9162b03903619d9956a01960d1596c5a
papillon <devopreet.ch@gmail.com>
2017-05-16 14:05:05 -0500

    *added mysql and dbcp drivers and urls to properties etc

---
+++ src/main/java/com/papillon/dc/OfferDAO.java
@@ -0,0 +1,13 @@
+package com.papillon.dc;
+
+import java.util.List;
+
+/**
+ * Created by papillon on 5/16/2017.
+ */
+public class OfferDAO {
+
+    public List<Offer> getOffers(){
+        return null;
+    }
+}
